1ad5d699a3f8beb86f9dd1cd1398b293c21a70f8
Ronay <oktemronay@gmail.com>
2025-01-01 20:25:15 +0300

    Enhance password security with BCrypt hashing
    Updated AuthController.cs to use BCrypt for password hashing and verification during login and registration. Modified OnlineKurs.csproj to include BCrypt.Net-Next package for this functionality.

--- OnlineKurs/Controllers/AuthController.cs
+++ OnlineKurs/Controllers/AuthController.cs
@@ -22,10 +22,15 @@ namespace OnlineKurs.Controllers
         public async Task<IActionResult> Login([FromBody] LoginDTO loginRequest)
         {
             var user = await _userRepository.GetUserByEmailAsync(loginRequest.Email);
-            if (user == null || user.Password != loginRequest.Password)
+            
+            if (user == null || !BCrypt.Net.BCrypt.Verify(loginRequest.Password, user.Password))
             {
                 return Unauthorized("Invalid credentials");
             }
+            //if (user == null || user.Password != loginRequest.Password)
+            //{
+            //    return Unauthorized("Invalid credentials");
+            //}
 
             var token = _jwtService.GenerateToken(user.Id, user.Role);
 
@@ -35,6 +40,8 @@ namespace OnlineKurs.Controllers
         [HttpPost("register")]
         public async Task<IActionResult> Register([FromBody] Users user)
         {
+            var hashedPassword = BCrypt.Net.BCrypt.HashPassword(user.Password);
+            user.Password = hashedPassword;
             await _userRepository.AddAsync(user);
             return Ok("User registered successfully");
         }
